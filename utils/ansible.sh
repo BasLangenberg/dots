3c08a3c812dcabceea1c5c83a62b59bca98be264
Bas Langenberg <baslangenberg@gmail.com>
2018-02-16 22:49:35 +0100

    Add 2 stage Ansible run for sudo rules to apply

--- utils/ansible.sh
+++ utils/ansible.sh
@@ -4,6 +4,7 @@ PLAYBOOK_DIR=/tmp/playbook
 
 git clone https://github.com/BasLangenberg/arch-install.git $PLAYBOOK_DIR
 cd $PLAYBOOK_DIR
+ansible-playbook -i hosts --tags install arch.yml
 ansible-playbook -i hosts arch.yml
 
 exit 0
