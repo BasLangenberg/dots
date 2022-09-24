6fbd462747305922551f774d22f702143ecddfdc
Bas Langenberg <bas.langenberg@cobase.com>
2022-09-24 11:34:07 +0200

    Update Ansible + bootstrap logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 apt update
 apt upgrade -y
 apt install -y git tmux zsh vim python3 python3-pip
-pip3 install ansible
+pip3 install --upgrade ansible molecule
 ansible-galaxy install gantsign.golang
 ansible-playbook -i hosts setup.yml
