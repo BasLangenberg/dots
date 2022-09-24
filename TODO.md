402f763ca8f41ad658f977cffd090a479c692a96
Bas Langenberg <bas.langenberg@cobase.com>
2022-09-24 14:16:03 +0200

    Add todo file, add setup precommit

---
+++ TODO.md
@@ -0,0 +1,11 @@
+# TODO
+
+## Add software
+- terraform versions
+- terragrunt
+- kubectx
+- kubens
+- chefdk
+- terragrunt-atlantis-config
+
+## Install Docker without using docker desktop
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,5 @@ apt install -y git tmux zsh vim python3 python3-pip
 pip3 install --upgrade ansible molecule
 ansible-galaxy install gantsign.golang
 ansible-playbook -i hosts setup.yml
+
+pre-commit install
