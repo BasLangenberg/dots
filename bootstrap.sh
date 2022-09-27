5d68189fe3baf5b16e6d3dad1fa18b70802e9ecd
Bas Langenberg <bas.langenberg@cobase.com>
2022-09-27 20:12:08 +0200

    Remove need for sudo within Ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-apt update
-apt upgrade -y
-apt install -y git tmux zsh vim python3 python3-pip
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git tmux zsh vim python3 python3-pip
 pip3 install --upgrade ansible molecule
 ansible-galaxy install -r roles/requirements.yml
-ansible-playbook -i hosts setup.yml
+ansible-playbook -i hosts setup.yml -K
 
 pre-commit install
