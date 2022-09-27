96a4b09eb0bb25ac5103d0a952eace620aa85d91
Bas Langenberg <bas.langenberg@cobase.com>
2022-09-27 20:36:15 +0200

    Fix path for pip

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,10 @@ sudo apt install -y git tmux zsh vim python3
 # Pip fixes & Ansible setup
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
-pip3 install cryptography --upgrade
-pip3 install pyOpenssl --upgrade
+~/.local/bin/pip3 install cryptography --upgrade
+~/.local/bin/pip3 install pyOpenssl --upgrade
 
-pip3 install --upgrade ansible molecule
+~/.local/bin/pip3 install --upgrade ansible molecule
 
 # Magic
 ~/.local/bin/ansible-galaxy install -r roles/requirements.yml
