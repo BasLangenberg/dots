5685e4a963692d00bae730d605f5e0ce479bacd7
Bas Langenberg <bas.langenberg@cobase.com>
2022-09-27 20:33:29 +0200

    Update bootstrap logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+# Updates, Upgrades and packages
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y git tmux zsh vim python3 python3-pip
+sudo apt install -y git tmux zsh vim python3
+
+# Pip fixes & Ansible setup
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+pip3 install cryptography --upgrade
+pip3 install pyOpenssl --upgrade
+
 pip3 install --upgrade ansible molecule
+
+# Magic
 ~/.local/bin/ansible-galaxy install -r roles/requirements.yml
 ~/.local/bin/ansible-playbook -i hosts setup.yml -K
 
+# Setup pre-commit
 pre-commit install
+
+exit 0
