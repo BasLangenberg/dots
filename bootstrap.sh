41b13cf330c5749855e1d97e739b7adacef3872b
Bas Langenberg <bas.langenberg@cobase.com>
2022-09-27 20:20:21 +0200

    Fix path issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y git tmux zsh vim python3 python3-pip
 pip3 install --upgrade ansible molecule
-ansible-galaxy install -r roles/requirements.yml
-ansible-playbook -i hosts setup.yml -K
+~/.local/bin/ansible-galaxy install -r roles/requirements.yml
+~/.local/bin/ansible-playbook -i hosts setup.yml -K
 
 pre-commit install
