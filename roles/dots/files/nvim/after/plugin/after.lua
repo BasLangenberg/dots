7f282aebd4bdd99d117ea8a3042b954cf76dbe9e
Bas Langenberg <baslangenberg@gmail.com>
2023-02-23 08:45:45 +0100

    Fix Lua in neovim

--- roles/dots/files/nvim/after/plugin/after.lua
+++ roles/dots/files/nvim/after/plugin/after.lua
@@ -94,12 +94,24 @@ local servers = {
   rust_analyzer = {},
   tsserver = {},
 
-  sumneko_lua = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
+  lua_ls = {
+    runtime = {
+      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+      version = 'LuaJIT',
     },
-  },
+    diagnostics = {
+      -- Get the language server to recognize the `vim` global
+      globals = {'vim'},
+    },
+    workspace = {
+      -- Make the server aware of Neovim runtime files
+      library = vim.api.nvim_get_runtime_file("", true),
+    },
+    -- Do not send telemetry data containing a randomized but unique identifier
+    telemetry = {
+      enable = false,
+    },
+  }
 }
 
 -- Ensure the servers above are installed
