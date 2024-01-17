ca806f1b97a56f203b79f23916b9278dd64c35f4
Bas Langenberg <bas.langenberg@cobase.com>
2024-01-17 11:20:33 +0100

    Update Telescope

--- roles/dots/files/nvim/lua/baslangenberg/plugins.lua
+++ roles/dots/files/nvim/lua/baslangenberg/plugins.lua
@@ -47,7 +47,7 @@ return require('packer').startup(function(use)
 
   -- Telescope
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
