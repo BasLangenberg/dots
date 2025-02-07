66db5f51f9b67ed3f4ba5261af8153a8d8adc6b1
Bas Langenberg <baslangenberg@gmail.com>
2025-02-07 17:45:11 +0100

    Add cloc

--- roles/dots/files/nvim/lua/vim-options.lua
+++ roles/dots/files/nvim/lua/vim-options.lua
@@ -78,18 +78,18 @@ vim.cmd([[
   set pastetoggle=<F10>
 
   set clipboard+=unnamedplus
-  let g:clipboard = {
-            \   'name': 'win32yank-wsl',
-            \   'copy': {
-            \      '+': 'win32yank.exe -i --crlf',
-            \      '*': 'win32yank.exe -i --crlf',
-            \    },
-            \   'paste': {
-            \      '+': 'win32yank.exe -o --lf',
-            \      '*': 'win32yank.exe -o --lf',
-            \   },
-            \   'cache_enabled': 0,
-            \ }
+"  let g:clipboard = {
+"            \   'name': 'win32yank-wsl',
+"            \   'copy': {
+"            \      '+': 'win32yank.exe -i --crlf',
+"            \      '*': 'win32yank.exe -i --crlf',
+"            \    },
+"            \   'paste': {
+"            \      '+': 'win32yank.exe -o --lf',
+"            \      '*': 'win32yank.exe -o --lf',
+"            \   },
+"            \   'cache_enabled': 0,
+"            \ }
   let g:loaded_perl_provider = 0
 
   " Persistent undo
